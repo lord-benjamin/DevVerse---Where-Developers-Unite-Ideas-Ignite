ea62113e28a8e100b0571844d7b44efff34b92b6
Dhruv Arora <98750226+lord-benjamin@users.noreply.github.com>
2024-01-03 23:49:00 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,24 @@
 import { defineConfig,loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import * as dotenv from 'dotenv';
-dotenv.config()
+// import * as dotenv from 'dotenv';
+// dotenv.config()
 
 // https://vitejs.dev/config/
 export default({mode}) => {
-  // process.env = {...process.env, ...loadEnv(mode, process.cwd())};
-  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  // process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
 
   return defineConfig({
     plugins: [react()],
     server: {
-      host: true,
+      // host: true,
       proxy: {
         '/api': {
           target: `${process.env.VITE_CL_DOMAIN}`,
           changeOrigin: true,
-          secure: false,
-          ws: true,
+          // secure: false,
+          // ws: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
         },
       },
     },

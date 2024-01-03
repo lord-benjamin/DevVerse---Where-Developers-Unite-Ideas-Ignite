22fd59672948b07d307a34319ce62223363c9a0c
Dhruv Arora <98750226+lord-benjamin@users.noreply.github.com>
2024-01-03 16:42:01 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    // port: 5173,
+    port: 5173,
     proxy: {
       '/api': {
         target: 'https://devverse-backend.cyclic.app',
